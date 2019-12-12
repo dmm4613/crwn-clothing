51d529b85cd3a2cd7482a8abfe97fd9b8f1fb79b
Dan <dmm4613@gmail.com>
2019-12-12 13:30:43 -0500

    updated directory reducer to allow menu items to link to appropriate locations

--- src/redux/directory/directory.reducer.js
+++ src/redux/directory/directory.reducer.js
@@ -3,19 +3,19 @@ const INITIAL_STATE = {
         title: 'hats',
         imageUrl: 'https://i.ibb.co/cvpntL1/hats.png',
         id: 1,
-        linkUrl: 'hats'
+        linkUrl: 'shop/hats'
     },
     {
         title: 'jackets',
         imageUrl: 'https://i.ibb.co/px2tCc3/jackets.png',
         id: 2,
-        linkUrl: ''
+        linkUrl: 'shop.jackets'
     },
     {
         title: 'sneakers',
         imageUrl: 'https://i.ibb.co/0jqHpnp/sneakers.png',
         id: 3,
-        linkUrl: ''
+        linkUrl: 'shop/sneakers'
 
     },
     {
@@ -23,7 +23,7 @@ const INITIAL_STATE = {
         imageUrl: 'https://i.ibb.co/GCCdy8t/womens.png',
         size: 'large',
         id: 4,
-        linkUrl: ''
+        linkUrl: 'shop/womens'
 
     },
     {
@@ -31,7 +31,7 @@ const INITIAL_STATE = {
         imageUrl: 'https://i.ibb.co/R70vBrQ/men.png',
         size: 'large',
         id: 5,
-        linkUrl: ''
+        linkUrl: 'shop/mens'
 
     }]
 };
